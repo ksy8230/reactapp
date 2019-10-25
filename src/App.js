84ec646687a4796897671747a742717ffc77c954
ksy8230 <ksy8230@hanmail.net>
2019-10-25 15:38:53 +0900

    react_movieapp_4.4강

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ class App extends React.Component {
       data : {
         data : { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     this.setState({movies : movies, isLoading : false})
-    console.log(movies)
+    // console.log(movies)
     // console.log(movies.data.data.movies)
   }
   componentDidMount(){
@@ -24,18 +24,29 @@ class App extends React.Component {
   }
   render(){
     const {isLoading, movies } = this.state;
-    return <div>
-    {isLoading ? "Loading..." 
-    : movies.map(movie =>{
-      return <Movie 
+    return (
+    <section class="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div class="movies">
+          {movies.map(movie =>{
+            return <Movie 
               key = {movie.id}
               id={movie.id} 
               year={movie.year} 
               title={movie.title} 
               summary={movie.summary} 
-              poster={movie.medium_cover_image} />
-    })}
-    </div>
+              poster={movie.medium_cover_image}
+              genres = {movie.genres}
+              />
+          })}
+        </div>
+        )
+      }
+    </section> )
   }
 }
 
